80e5ca00650f2cc7fb3d0d426bb99207f2756c60
zahid-akhtar7979 <73689151+zahid-akhtar7979@users.noreply.github.com>
2021-07-14 08:45:43 +0530

    added method to reverse string using char array

--- data-structures/src/com/java/strings/ReverseString.java
+++ data-structures/src/com/java/strings/ReverseString.java
@@ -3,9 +3,10 @@ package com.java.strings;
 public class ReverseString {
 	
 	public static void main(String[] args) {
-		System.out.println(reverseString("hello"));
+		System.out.println(reverseStringUsingCharArray("hello"));
 	}
-
+	
+	//using StringBuilder
 	private static String reverseString(String str) {
 		
 		StringBuilder reversedString = new StringBuilder();
@@ -13,5 +14,18 @@ public class ReverseString {
 			reversedString = reversedString.append(str.charAt(i));
 		}
 		return reversedString.toString();
+	}
+	
+	//using char array
+	private static String reverseStringUsingCharArray(String str) {
+		
+		char[] charArray = str.toCharArray();
+		
+		for(int i = 0 ,j = charArray.length-1; i<j; i++, j--) {
+			char temp = charArray[i];   
+			charArray[i] = charArray[j];
+			charArray[j] = temp;
+		}
+		return String.valueOf(charArray);
 	}
 }
